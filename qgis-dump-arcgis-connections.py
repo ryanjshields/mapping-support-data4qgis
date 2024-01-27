e5b93242656d3d8d5c68646dd4e4743488e2483f
Ryan <shieldsjryan@gmail.com>
2024-01-26 22:46:06 -0700

    updated for compatibility with current QGIS releases

--- qgis-dump-arcgis-connections.py
+++ qgis-dump-arcgis-connections.py
@@ -1,16 +1,28 @@
 """
-This script should be run from the Python consol inside QGIS.
+This script should be run from the Python console inside QGIS.
 
-Removes all connections from the QGIS browser that are ArcGIS Feature Server types. Written as part of a workflow to pull in and remove serers from mappingsupport.com
+Removes all connections from the QGIS browser that are ArcGIS Feature Server types. Written as part of a workflow to pull in and remove servers from mappingsupport.com
 
 Script by Ryan Shields
 
 Licence GPLv3
 """
 
+print("Starting script...")
+
+# Create an instance of QSettings
 settings = QSettings()
-settings.beginGroup('qgis/connections-arcgisfeatureserver')
+
+# Begin a new group with the name 'connections/arcgisfeatureserver'
+settings.beginGroup('connections/arcgisfeatureserver')
+
+# Remove all keys and values from the current group
 settings.remove("")
-settings.allKeys()
+
+# End the current group
 settings.endGroup()
+
+# Reload the QGIS browser to reflect the changes
 iface.reloadConnections()
+
+print("Connections removed...")
